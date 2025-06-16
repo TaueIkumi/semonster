660b8446e29efaee0d90108cf00161c4475eda9d
taueikumi <e1q21054@oit.ac.jp>
2025-06-16 11:34:55 +0900

    fix

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMONSTER GAME");
-    System.out.println("Show a new monster");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
+    System.out.println("Show draw monsters!");
+    Monster[] monsters = new Monster[5];
+    for (int i = 0; i < monsters.length; i++) {
+      monsters[i] = new Monster();
+      System.out.println(monsters[i]);
+    }
+
   }
 }
